f87d1dd881258308e497063765f5abc85fd603ad
david <david.miranda@residentadvisor.net>
2017-07-31 19:16:15 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string command = "";
+
+            while (command != "exit")
+            {
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
